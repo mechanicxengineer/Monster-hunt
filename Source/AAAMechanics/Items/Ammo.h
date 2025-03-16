8bebb88023dbcb19e0280b34b7c6b3466259dd52
mechanicxengineer <monumanish500@gmail.com>
2025-03-16 17:26:28 +0530

    Imported new assests

---
+++ Source/AAAMechanics/Items/Ammo.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/Item.h"
+#include "Ammo.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AAAMECHANICS_API AAmmo : public AItem
+{
+	GENERATED_BODY()
+	
+};
